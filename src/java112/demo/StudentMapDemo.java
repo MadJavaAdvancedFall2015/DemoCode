310c38e9722962d22d0cb05b02fabfaef521ac54
Paula Waite <pawaite@madisoncollege.edu>
2015-10-15 15:35:54 -0500

    adding map demo

---
+++ src/java112/demo/StudentMapDemo.java
@@ -0,0 +1,29 @@
+package java112.demo;
+
+import java.util.*;
+
+/** A class to demonstrate maps containing objects
+ *  @author pwaite
+ */
+
+ public class StudentMapDemo {
+
+ /** Main method to demonstrate use of the Treemap class to
+  *  hold a sorted set of mapped data.
+  *  @param args Parameters that are entered on the command line.
+  */
+
+  public static void main(String args[]) {
+
+      // create a map
+      Map students = new TreeMap();
+
+
+
+  }
+
+
+
+
+
+ }
