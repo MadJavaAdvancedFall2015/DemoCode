64a99ac8bfbf04b4ab3390c16b679d1c2aba9813
Paula Waite <pawaite@madisoncollege.edu>
2015-11-17 14:59:42 -0600

    adding example for updating a value in the map - refer to StudentMapDemo

--- src/java112/demo/StudentMapDemo.java
+++ src/java112/demo/StudentMapDemo.java
@@ -16,9 +16,22 @@ import java.util.*;
   public static void main(String args[]) {
 
       // create a map
-      Map students = new TreeMap();
+      Map<Integer, Student> students = new TreeMap<Integer, Student>();
 
+      students.put(1, new Student("Jim", "G", "A", 1));
+      students.put(2, new Student("Joe", "P", "A", 2));
+      students.put(3, new Student("Joe", "P", "A", 3));
 
+      System.out.println(students);
+
+      int studentId = 1;
+      // update an object in the map
+      if (students.containsKey(studentId)) {
+           Student student = students.get(studentId);
+           student.setFirstName("Suzie");
+      }
+      // notice that student 1's name is updated...
+      System.out.println(students);
 
   }
 
